118dd8972761040ac17bab9827cc48b7fedd78f3
Vestial <penskewilliano@gmail.com>
2018-03-29 12:08:36 +0200

    Add shuffle, addCount, addMoves, and reload to gameController

--- js/app.js
+++ js/app.js
@@ -113,6 +113,30 @@ const gameController = {
     },
     getStars() {
         return gameState.stars;
+    },
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+    
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+    
+        return array;
+    },
+    addCount() {
+        return memoryModel.counts += 2;
+    },
+
+    addMoves() {
+        return memoryModel.moves += 1;
+    },
+    reloadGame() {
+        location.reload();
     }
 };
 /*
@@ -121,21 +145,6 @@ const gameController = {
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 
 /*
